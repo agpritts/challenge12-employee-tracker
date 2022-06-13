6e2e4659644b43b687467946ac04365ba02eb9b3
erma <agpritts@gmail.com>
2022-06-13 00:01:47 -0400

    sql debug

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 
 const connection = mysql.createConnection({
   host: 'localhost',
-  port: 3023,
+  port: 3306,
   user: 'root',
   password: process.env.MYSQL_PASSWORD,
   database: 'employees_db'
